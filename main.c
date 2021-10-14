454bcd2c3e10c9918195159b9e739e6aa11f61df
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-10-14 12:54:23 -0400

    initial commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/** ./main.c
+ * Runs an example of testing a simple function.
+ * By      : Leomar Duran <https://github.com/lduran2/>
+ * When    : 2021-10-14t12:52
+ * For     : CIS 3207 Assignment 4
+ * Version : 1.0
+ * Canonical: <>
+ *
+ * CHANGELOG:
+ *     v1.0 - 2021-10-14t12:52
+ *         initial commit
+ */
+int main(int argc, char ** argv) {
+	return 0;
+} /* int main(int argc, char ** argv) */
+
