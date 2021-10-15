839c8af4f13b8bbcb21c136e95c66f464a1669bb
Leomar Durán <29983534+lduran2@users.noreply.github.com>
2021-10-15 05:30:22 -0400

    implemented time ellapsed calculation

--- demo.c
+++ demo.c
@@ -1,12 +1,15 @@
 /** ./demo.c
  * Runs an example of testing a simple function.
  * By      : Leomar Duran <https://github.com/lduran2/>
- * When    : 2021-10-14t22:36
+ * When    : 2021-10-15t03:49
  * For     : CIS 3207 Assignment 4
- * Version : 1.0
+ * Version : 1.1
  * Canonical: <https://github.com/lduran2/c-function-timer/blob/master/demo.c>
  *
  * CHANGELOG:
+ *     v1.1 - 2021-10-15t03:49
+ *         print the average time elapsed
+ *
  *     v1.0 - 2021-10-14t12:52
  *         testing the printf
  *
@@ -18,8 +21,10 @@
 #include "fntimer.h"	/* for fntime */
 
 int main(int argc, char ** argv) {
+	double printf_test;
 	/* test 5 calls to printf */
-	fntime(5, (Timeable *)printf, "Hello world!\n");
+	printf_test = fntime(5, (Timeable *)printf, "Hello world!\n");
+	printf("%f\n", printf_test);
 	return 0;
 } /* int main(int argc, char ** argv) */
 
--- fntimer.c
+++ fntimer.c
@@ -1,12 +1,15 @@
 /** ./fntimer.c
  * Times a given function.
  * By      : Leomar Duran <https://github.com/lduran2/>
- * When    : 2021-10-14t22:35
+ * When    : 2021-10-15t03:48
  * For     : CIS 3207 Assignment 4
- * Version : 1.0
+ * Version : 1.1
  * Canonical: <https://github.com/lduran2/c-function-timer/blob/master/fntimer.c>
  *
  * CHANGELOG:
+ *     v1.1 - 2021-10-15t03:48
+ *         implemented calculating the time elapsed
+ *
  *     v1.0 - 2021-10-14t22:35
  *         attempt using single void pointer
  *
@@ -14,15 +17,28 @@
  *         attempt using varargs
  */
 
-#include "fntimer.h"
+#include "fntimer.h"	/* the header */
+#include <stdlib.h>	/* for NULL */
 #include <sys/time.h>	/* for struct timeval */
-#include <stdio.h>
 
 double fntime(int n_reps, Timeable func, void * arg) {
-	double mean_time = 0.0;
+	struct timeval start_timeval;	/* time that loop started */
+	struct timeval last_timeval;	/* current time */
+	double time_delta = 0.0;
+
+	/* for each number in the test */
 	while (n_reps--) {
+		/* get the starting time */
+		gettimeofday(&start_timeval, NULL);
+		/* perform the task */
 		func(arg);
-	};
-	return mean_time;
-} /* int fntime(int n_reps, Timeable func, void * arg) */
+		/* get the ending time */
+		gettimeofday(&last_timeval, NULL);
+		/* update the time delta */
+		time_delta += (double)(last_timeval.tv_sec - start_timeval.tv_sec)
+			+ ((double)(last_timeval.tv_usec - start_timeval.tv_usec)/1E+06);
+	}; /* while (n_reps--) */
+
+	return time_delta/n_reps;
+} /* double fntime(int n_reps, Timeable func, void * arg) */
 
